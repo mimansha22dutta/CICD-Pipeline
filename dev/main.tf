9d5a559f28facbf4693509897f30dfa2555367b8
Mimansha Dutta <mimanshaedu@gmail.com>
2025-09-04 04:57:33 +0530

    dev and prod rg added

--- dev/main.tf
+++ dev/main.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "resource-group-we-env"
+  name     = "resource-group-we-dev"
   location = "West Europe"
 }
 
 # second resource group for testing purposes
 resource "azurerm_resource_group" "rg2" {
-  name     = "resource-group-ci-env"
+  name     = "resource-group-ci-dev"
   location = "Central India"
 }
